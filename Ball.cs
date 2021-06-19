fc766702768a0a574f510555bd9a9136caf7979a
trongnv20172866 <83620961+trongnv20172866@users.noreply.github.com>
2021-06-19 19:03:54 +0700

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    GameController m_gc;
+    GameController m_gc; // tham chieu den script GameController va dat ten la m_gc
 
     private void Start()
     {
-        m_gc = FindObjectOfType<GameController>();
+        m_gc = FindObjectOfType<GameController>(); // tham chiu thuong phai find object of type
     }
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D col) // Ham va cham 2d theo kieu va cham vat ly
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player")) // neu col co kieu Collision2D tham chieu den gameobject co tag la Player
         {
-            m_gc.IncrementScore();
-            Destroy(gameObject);
-            Debug.Log("Va cham");
+            m_gc.IncrementScore(); // Game controller tham chieu den Ham IncrementScore
+            Destroy(gameObject); // Ham xoa doi tuong co tag la player
+            Debug.Log("Va cham"); // kiem tra da va cham hay chua
         }
        
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col) // Ham va cham 2d co kieu phi vat ly (xuyen qua)
     {
-        if(col.CompareTag("DeathZone"))
+        if(col.CompareTag("DeathZone")) // neu col co kieu colider 2d ma cham den vung co tag la deathzone
         {
             m_gc.SetGameoverState(true);
             Destroy(gameObject);

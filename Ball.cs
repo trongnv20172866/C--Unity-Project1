86c106ad4bac259ce3bb6f363736590a5ae5032b
trongnv20172866 <83620961+trongnv20172866@users.noreply.github.com>
2021-06-19 17:41:44 +0700

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -1,1 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Ball : MonoBehaviour
+{
+    GameController m_gc;
+
+    private void Start()
+    {
+        m_gc = FindObjectOfType<GameController>();
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            m_gc.IncrementScore();
+            Destroy(gameObject);
+            Debug.Log("Va cham");
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("DeathZone"))
+        {
+            m_gc.SetGameoverState(true);
+            Destroy(gameObject);
+            Debug.Log("Game over");
+        }
+    }
+
+}
